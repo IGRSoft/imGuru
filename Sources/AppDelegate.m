6c035a58abe78aed38ad501ac2e99e7af5395a3c
Vitalii Parovishnyk <Korich.vi.p@gmail.com>
2015-03-04 23:56:08 +0200

    Added Auth via Pin

--- Sources/AppDelegate.m
+++ Sources/AppDelegate.m
@@ -7,23 +7,37 @@
 //
 
 #import "AppDelegate.h"
+#import "ImgurSession.h"
 
-@interface AppDelegate ()
+#define ClientID @"bc5111e191dfa20"
+#define ClientSecret @"a76a2be0cf75660e2dade7a86a6b2371c4cb9e95"
+
+
+@interface AppDelegate () <IMGSessionDelegate>
 
 @property (weak) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSButton *authButton;
+@property (weak) IBOutlet NSTextField *stateLabel;
+
+@property (strong) IMGSession *imgSession;
+@property (copy) void(^continueHandler)();
+@property (assign) BOOL waitingAuth;
+
+- (IBAction)authorizeTapped:(id)sender;
+
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    
+    self.waitingAuth = NO;
+    [IMGSession anonymousSessionWithClientID:ClientID withDelegate:self];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
 }
-
-BOOL doNothingAtStart = NO;
 
 - (void)applicationDidBecomeActive:(NSNotification *)notification
 {
@@ -35,8 +49,58 @@ BOOL doNothingAtStart = NO;
         return;
     }
     
+    if (self.waitingAuth)
+    {
+        NSString *pin = [[NSPasteboard generalPasteboard] stringForType:NSStringPboardType];
+        
+        if (pin.length == 10)
+        {
+            self.waitingAuth = NO;
+            
+            [self.imgSession setAuthenticationInputCode:pin];
+            [self.imgSession authenticate];
+        }
+    }
+    
     [self.window makeKeyAndOrderFront:self];
     [self.window center];
+}
+
+- (IBAction)authorizeTapped:(id)sender {
+    
+    if([IMGSession sharedInstance].isAnonymous) {
+        
+        self.waitingAuth = YES;
+        self.stateLabel.stringValue = @"Logging In";
+        
+        //set your credentials to reset the session to your app
+        self.imgSession = [IMGSession authenticatedSessionWithClientID:ClientID
+                                                                secret:ClientSecret
+                                                              authType:IMGPinAuth
+                                                          withDelegate:self];
+        [self.imgSession authenticate];
+        
+    }
+}
+
+-(void)imgurSessionNeedsExternalWebview:(NSURL*)url completion:(void(^)())completion
+{
+    NSLog(@"url - %@", url);
+    
+    self.continueHandler = [completion copy];
+    
+    //go to safari to login, configure your imgur app to redirect to this app using URL scheme.
+    [[NSWorkspace sharedWorkspace] openURL:url];
+}
+
+-(void)imgurSessionAuthStateChanged:(IMGAuthState)state;
+{
+    
+}
+
+-(void)imgurSessionUserRefreshed:(IMGAccount*)user
+{
+    self.stateLabel.stringValue = user.username;
 }
 
 @end
